e7b8d2b9de1743be6e163813cace8c48ffbb54db
Yerragorla Ganesh Yadav <yerragorlaganeshyadav@Yerragorlas-MacBook-Pro.local>
2025-06-12 15:45:40 +0530

    code added to improve UI

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,18 @@ import AboutSection from "./components/AboutSection";
 import EventSection from "./components/EventSection";
 import Footer from "./components/Footer";
 import PhotosSection from "./components/PhotosSection";
+import Sampada from "./components/Sampada";
 
 function App() {
   return (
     <div className="flex flex-col">
-      <Header />
+      {/* <Header />
       <HomeSection />
       <AboutSection />
       <PhotosSection />
       <EventSection />
-      <Footer />
+      <Footer /> */}
+      <Sampada />
     </div>
   );
 }
